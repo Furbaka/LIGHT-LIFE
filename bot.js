237e2e7fd8bcfbea9a7e87cdda2e4ab9873248ab
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-18 12:43:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,36 +17,5 @@ client.on('message', message => {
   	}
 });
 
-bot.login("NDI0NjIxMTg2NzM2MDYyNDY1.DY_NkA.dib6aWGHgBkaJVmhrzxdsUz6y0E");
-var prefix = ("[");
-
-bont.on("message", message => {
-	if(message.content --- prefix + "info") {
-		var embed = new Discord.RichEmbed()
-		.setDescription("Informations du serveur")
-		.addField("Nom du serveur", message.guild.name)
-		.addfield("Créé le", message.guild.createdAt)
-		.addField("Utilisateurs sur le serveurs", message.guild.membercount)
-		.setColor("0x0000FF")
-	message.channel.sendEmbed(embed)
-
-	}
-
-	if (message.content.startWith(prefix + "sondage")) {
-		if(message.author.is == "3201325011210310325"){
-			let args = message.content.split(" ").slice(1);
-			let thingToEcho = args.join(" ")
-			var embed = new Discord.RichEmbed()
-				.setDescription("Sondage")
-				.addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-				.setColor("0xB40404")
-				.setTimestamp()
-			message.react("✔")
-			message.react("❌")
-		}).catch(function() {
-		));
-		}else{
-			return message.reply("Tu n'as pas la persmission.")
-}}}}
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
